e03597f722e3ee3945982c470ae67295c16ad77f
Cody Wahl <codywahl@gmail.com>
2016-11-17 15:03:42 -0800

    Completed matching algorithms by event type and time
    Does not accout for max group size or persist created groups so that
    others can join before the event starts.

---
+++ src/SlalomConnectsAPI/Models/EventGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlalomConnectsAPI.Models
+{
+    public class EventGroup
+    {
+        public List<EventRequest> EventRequests { get; set; }
+        public EventType EventType { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
