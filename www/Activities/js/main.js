d7f9125542711f02b7e886e26eb40beba45fb1af
Brian Chia <brian.chia@slalom.com>
2016-11-18 10:39:54 -0800

    pulled data value from forms. need to connect to api endpoint

--- www/Activities/js/main.js
+++ www/Activities/js/main.js
@@ -1,30 +1,53 @@
-function setSize() {
+var email;
+var eventType;
+var startTime;
+var endTime;
+var maximumGroupSize;
+
+
+function setSize(event) {
     document.getElementById("activity-options").style.display = "none";
     document.getElementById("size").style.display = "block";
+    eventType = event;
+
+    console.log(eventType);
 }
 
 function setTime() {
     document.getElementById("size").style.display = "none";
     document.getElementById("time").style.display = "block";
+    maximumGroupSize = document.getElementById("maximumGroupSize").value;
+
+    console.log(maximumGroupSize);
 }
 
 function createUser() {
     document.getElementById("time").style.display = "none";
     document.getElementById("validUser").style.display = "block";
+    startTime = document.getElementById("startTime").value;
+    endTime = document.getElementById("endTime").value;
+
+    console.log(startTime);
+    console.log(endTime);
+}
+
+
+function submitForm() {
+    name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+
+    console.log(name + " " + email + " " + password);
+
+    //document.getElementById("myForm").submit();
+
 }
 
 
 
 
-//var event = {
-//    name;
-//    groupSize;
-//    startTime;
-//    endTime;
-//}
-//
-//var user {
-//    slalomEmail;
-//    slalomPassword;
-//    name;
-//}
+
+//    <form id="myForm" method="post" action="slalom-connects-api/post-event-request">
+
+//    public HttpResponseMessage Post(string email, EventType eventType, DateTime startTime, DateTime endTime, int? maximumGroupSize)
+
